58a29de49b78a168f79ee48b14e1949bc8632eff
Viktoriia Bieltsova <vikav0206@gmail.com>
2023-01-24 21:01:49 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtnEl = document.querySelector('button[data-start]');
+const stopBtnEl = document.querySelector('button[data-stop]');
+let timerId = null;
+
+startBtnEl.addEventListener('click', () => {
+  startBtnEl.setAttribute('disabled', true);
+  stopBtnEl.removeAttribute('disabled');
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopBtnEl.addEventListener('click', () => {
+  clearInterval(timerId);
+  stopBtnEl.setAttribute('disabled', true);
+  startBtnEl.removeAttribute('disabled');
+});
